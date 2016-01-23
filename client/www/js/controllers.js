1537359e7c29d56d7626c8388c7750c56b818666
preeya <hpsjoshi@gmail.com>
2016-01-23 19:26:33 +0000

    Init OAuth implementation

--- client/www/js/controllers.js
+++ client/www/js/controllers.js
@@ -1,7 +1,6 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['login'])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, AuthService) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -26,18 +25,12 @@ angular.module('starter.controllers', [])
 
   // Open the login modal
   $scope.login = function() {
-    $scope.modal.show();
+    AuthService.login('mondo');
   };
 
   // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
     console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
   };
 })
 
